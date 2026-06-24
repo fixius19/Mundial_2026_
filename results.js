9f4263f2563e87a1e639124ce84572967381e5c5
fixius19 <santiagocallon@gmail.com>
2026-06-24 10:36:56 +0200

    Update results.js

--- results.js
+++ results.js
@@ -170,15 +170,15 @@ const RESULTS = {
       "Portugal__Colombia": { "home": null, "away": null },
       "Uzbekistan__DR Congo": { "home": null, "away": null },
       "Uzbekistan__Colombia": { "home": 1, "away": 3 },
-      "DR Congo__Colombia": { "home": null, "away": null }
+      "DR Congo__Colombia": { "home": 1, "away": 0 }
     },
     "L": {
-      "England__Ghana": { "home": null, "away": null },
+      "England__Ghana": { "home": 0, "away": 0 },
       "England__Croatia": { "home": 4, "away": 2 },
       "England__Panama": { "home": null, "away": null },
       "Ghana__Croatia": { "home": null, "away": null },
       "Ghana__Panama": { "home": 1, "away": 0 },
-      "Croatia__Panama": { "home": null, "away": null }
+      "Croatia__Panama": { "home": 0, "away": 1 }
     }
   },
   "knockout": {
